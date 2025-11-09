ce0bc279c69f985373a767e9a06f084ad0b67e18
sadiabinta <sadiaa.kabir@gmail.com>
2025-11-09 18:58:58 -0500

    problem solving

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -8,50 +8,6 @@ export default function Register() {
       <div className="absolute z-10 top-0 left-0">
         <RegistrationForm />
       </div>
-      {/* <div className="absolute inset-0 bg-black/40">
-        <Card className="relative z-10 w-[350px] bg-sky-800 opacity-70 shadow-xl mt-16 mx-16">
-          <CardHeader>
-            <CardTitle className="text-center">
-              Register to create an account
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="flex flex-col gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                  </div>
-                  <Input id="password" type="password" required />
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <Button type="submit" className="w-full">
-              Register
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-            <p>
-              Already Have an Account? Please{" "}
-              <Link to="/login" className="text-green-600">
-                Login
-              </Link>
-            </p>
-          </CardFooter>
-        </Card>
-      </div> */}
 
       <div className="w-full h-screen">
         <img
