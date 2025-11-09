311fcc654c56b04147806365999683eb4a29ffcd
sadiabinta <sadiaa.kabir@gmail.com>
2025-11-09 16:52:28 -0500

    deploying

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
-import Banner from "@/components/modules/homepage/banner";
+import Banner from "@/components/modules/homepage/Banner";
 import CountSec from "@/components/modules/homepage/CountSec";
 import HeroSection from "@/components/modules/homepage/HeroSection";
-import Testimonial from "@/components/modules/homepage/testimonial";
+import Testimonial from "@/components/modules/homepage/Testimonial";
 
 export default function HomePage() {
   return (
